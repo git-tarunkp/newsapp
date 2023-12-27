af6b56e2f09e3c2ece43630445333efd40d01b5b
TARUN <108211464+git-tarunkp@users.noreply.github.com>
2023-12-28 00:44:57 +0530

    Update home.js

--- project1deplo/src/components/home.js
+++ project1deplo/src/components/home.js
@@ -10,7 +10,7 @@ const Home = () => {
   useEffect(() => {
 
     const getArticles = async () => {
-      const response = await axios.get('https://newsdata.io/api/1/news?apikey=pub_354832adfb850bb2618843bef68de0f95c1ac&q=sports%20news&country=in&language=en&category=education,other,politics ')
+      const response = await axios.get('https://newsdata.io/api/1/news?apikey=pub_35493a04c74d2a2553389c2205bddad0c51b1&q=sports%20news&country=in&language=en&category=education,other,politics ')
       console.log(response)
       setArticles(response.data.results)
     }
